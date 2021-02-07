58ea41a1a83c7e2337ed65b829a2b6c5f62e7708
Abdulazizsayed <Abdulazizsayed>
2021-02-07 17:32:45 +0200

    Updating messages link to be fillable and messages to be seen if the user view it

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -7,6 +7,7 @@ use App\Image;
 use App\Message;
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class MessageController extends Controller
 {
@@ -17,6 +18,7 @@ class MessageController extends Controller
      */
     public function index(User $user)
     {
+        DB::table('messages')->whereIn('sender_id', [auth()->user()->id, $user->id])->whereIn('receiver_id', [auth()->user()->id, $user->id])->update(['seen' => 1]);
         return view('users.messages')->with([
             'messages' => Message::whereIn('sender_id', [$user->id, auth()->user()->id])->whereIn('receiver_id', [$user->id, auth()->user()->id])->orderBy('created_at', 'desc')->get(),
             'lastPeopleMessagedMe' => Message::where('sender_id', auth()->user()->id)->orWhere('receiver_id', auth()->user()->id)->orderBy('created_at', 'desc')->get()->map(function ($message) {
